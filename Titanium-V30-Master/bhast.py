f4314eec7889fcfd9a910a7ebb807b425f42f0da
pityumajsaii-cell <pityumajsaii@gmail.com>
2026-03-17 23:37:17 +0100

    TITANIUM APEX: FULL SYSTEM SAVE [Tue Mar 17 23:37:08 CET 2026]

--- Titanium-V30-Master/bhast.py
+++ Titanium-V30-Master/bhast.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import threading
+import time
 
 def initialize():
     print("[BHAST] Engine Iron-Clad 4.3 - Advanced Core Active")
@@ -12,10 +12,8 @@ class Server:
         print(f"[BHAST] Flask Server mapped to {host}:{port}")
 
     def route(self, rule, **options):
-        # Ez teszi lehetővé az @server.route("/") használatát
         return self.app.route(rule, **options)
 
     def run(self):
         print(f"[BHAST] Launching Titanium Control Center...")
-        # A Hugging Face a 7860-as portot várja
         self.app.run(host=self.host, port=self.port, debug=False, use_reloader=False)
