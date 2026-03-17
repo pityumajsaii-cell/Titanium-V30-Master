edcc9b86db35393d0f20a0fd0aa80f0c99b2d971
Pityumajsaii-cell <istvanmajsai70pixel@gmail.com>
2026-03-17 21:41:44 +0100

    Titanium V30: 24/7 Keep-Alive aktiválva

--- Titanium-V30-Master/app.py
+++ Titanium-V30-Master/app.py
@@ -1,17 +1,31 @@
 import os
+import time
+import threading
+import requests
 from flask import Flask
 
 app = Flask(__name__)
+
+def keep_alive():
+    while True:
+        try:
+            # Önmaga meghívása, hogy ne aludjon el a Space
+            requests.get("http://localhost:7860/")
+        except:
+            pass
+        time.sleep(600) # 10 perc
 
 @app.route("/")
 def home():
     return {
         "status": "ONLINE",
-        "system": "TITANIUM V30",
-        "user": "Titanium",
+        "system": "TITANIUM V30 MASTER",
+        "owner": "Titanium",
         "payout_target": "Revolut LT81 3250...",
-        "mode": "MAX MODE 40% BOOST"
+        "uptime": "24/7 ACTIVE"
     }
 
 if __name__ == "__main__":
+    # Keep-alive szál indítása
+    threading.Thread(target=keep_alive, daemon=True).start()
     app.run(host='0.0.0.0', port=7860)
